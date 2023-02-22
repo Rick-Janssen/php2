05cb6029938f84b7cd2a347ef145d229d633c8fb
Rick-Janssen <rick.janssen3107@gmail.com>
2023-02-22 12:17:31 +0100

    dv

--- create.php
+++ create.php
@@ -44,7 +44,7 @@
             $sql = "INSERT INTO inlog (id, username, password)
     VALUES ('','$createUsername','$createPassword')";
             if ($conn->query($sql) === TRUE) {
-                $reroute  =  '<script>alert("account created <br> log in again")</script>';
+                $reroute  =  '<script>alert("account created log in again")</script>';
                 $reroute .= "<script>location.href='index.php'</script>";
                 echo $reroute;
             } else {
