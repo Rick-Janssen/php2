c77492665d813eae5259d065b292ff7bceb4d321
Rick-Janssen <rick.janssen3107@gmail.com>
2023-03-07 09:30:07 +0100

    gg

--- create.php
+++ create.php
@@ -31,7 +31,6 @@
     </section>
 </body>
     <?php
-
     $conn = mysqli_connect('rdbms.strato.de', 'dbu1035725', 'Stoeptegel3!3107', 'dbs10066227');
     if ($conn === false) {
         die("ERROR: Could not connect. " . mysqli_connect_error());
--- welcome.php
+++ welcome.php
@@ -1,59 +1,64 @@
-<link rel="stylesheet" type="text/css" href="stylewelcome.css">
+<link rel="stylesheet" type="text/css" href="welcome.css">
 <title>Rick janssen</title>
 <?php
-$conn = mysqli_connect('rdbms.strato.de', 'dbu1035725', 'Stoeptegel3!3107', 'dbs10066227');
-if (!$conn) {
-    die('Connection Failed' . mysqli_connect_error());
-}
-
-$result = mysqli_query($conn, 'SELECT * FROM `inlog`');
-
-$collection = [];
-if (mysqli_num_rows($result) > 0)
-    ; {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $collection[] = $row;
+    SESSION_start();
+    $conn = mysqli_connect('rdbms.strato.de', 'dbu1035725', 'Stoeptegel3!3107', 'dbs10066227');
+    if (!$conn) {
+        die('Connection Failed' . mysqli_connect_error());
     }
-}
-// $htmlTable = "<table>";
-// foreach ($collection as $value) {
-//     $htmlTable .= "<tr><td class='info'>" . $value['id'] . "</td>";
-//     $htmlTable .= "<td class='info'>" . $value['username'] . "</td>";
-//     $htmlTable .= "<td class='info'>" . $value['password'] . "</td>";
 
-// }
-// $htmlTable .= "</table>";
+    $result = mysqli_query($conn, 'SELECT * FROM `inlog`');
 
-//echo ($render);
-//echo "<pre>"; var_dump($_POST); echo "</pre>";
-//echo "<pre>"; var_dump($collection); echo "</pre>";
-//die();
-
-// if(!empty($_POST['Login'])){ // check of op knop is gedrukt
-// if (empty($_SESSION['status'])) {
-if (isset($_POST['password']) && (isset($_POST['username']))) {
-    $bInlogOke = false;
-    $ingelogde = null;
-    foreach ($collection as $person) {
-        // echo md5($_POST["password"]);
-        // echo "- " . $person['password'];echo "<br>";
-        if ((md5($_POST["password"]) == $person['password']) && ($_POST["username"] == $person['username'])) {
-            $bInlogOke = true;
-            $ingelogde = $person;
+    $collection = [];
+    if (mysqli_num_rows($result) > 0)
+        ; {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $collection[] = $row;
         }
     }
-    if ($bInlogOke) {
-        echo "<h2>" . $ingelogde['username'] . " is ingelogd!</h2>" . $htmlTable;
-    } else {
-        $foutmelding = '<script>alert("username or password incorrect!")</script>';
-        $foutmelding .= "<script>location.href='index.php'</script>";
-        echo $foutmelding;
-    }
-} 
-// } 
-// }  
-?>
+    // $htmlTable = "<table>";
+    // foreach ($collection as $value) {
+    //     $htmlTable .= "<tr><td class='info'>" . $value['id'] . "</td>";
+    //     $htmlTable .= "<td class='info'>" . $value['username'] . "</td>";
+    //     $htmlTable .= "<td class='info'>" . $value['password'] . "</td>";
 
-<h2><a href="http://stefanmars.nl">stefan mars</a></h2><br><br><br><br><br><br><br><br><br><br><br>
-<h2><a href="index.php">Logout</a></h2>
-<h2><a href="message.php">chat</a></h2>
+    // }
+    // $htmlTable .= "</table>";
+
+    //echo ($render);
+    //echo "<pre>"; var_dump($_POST); echo "</pre>";
+    //echo "<pre>"; var_dump($collection); echo "</pre>";
+    //die();
+
+    // if(!empty($_POST['Login'])){ // check of op knop is gedrukt
+    // if (empty($_SESSION['status'])) {
+    if (isset($_POST['password']) && (isset($_POST['username']))) {
+        $bInlogOke = false;
+        $ingelogde = null;
+        foreach ($collection as $person) {
+            // echo md5($_POST["password"]);
+            // echo "- " . $person['password'];echo "<br>";
+            if ((md5($_POST["password"]) == $person['password']) && ($_POST["username"] == $person['username'])) {
+                $bInlogOke = true;
+                $ingelogde = $person;
+            }
+        }
+        if ($bInlogOke) {
+            echo "<h2>" . $ingelogde['username'] . " is ingelogd!</h2>" . $htmlTable;
+        } else {
+            $foutmelding = '<script>alert("username or password incorrect!")</script>';
+            $foutmelding .= "<script>location.href='index.php'</script>";
+            echo $foutmelding;
+        }
+    } 
+    // } 
+    // }  
+    $render  ='<h3><a href="http://stefanmars.nl">stefan mars</a></h3><br><br><br><br><br><br><br><br><br><br><br>';
+    $render .='<form action="" method=""><button type="submit" id ="logout" value ="logout" name ="logout">logout</button></form>';
+    $render .='<h3><a href="message.php">chat</a></h3>';
+    echo $render;
+
+    if(!empty($post['logout'])){
+        header('Location: https://rickjanssenic1e.nl/');
+        $_SESSION = [];
+    }
