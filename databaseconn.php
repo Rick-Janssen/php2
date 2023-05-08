1cabaebdcdd3d0a82f25e6984e0a9502d6ed6c6c
Rick-Janssen <rick.janssen3107@gmail.com>
2023-05-08 10:56:03 +0200

    rtjk

--- databaseconn.php
+++ databaseconn.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect('rdbms.strato.de', 'dbu1035725', 'Stoeptegel3!3107', 'dbs10066227');
-#$conn = mysqli_connect('localhost', 'root','', 'php');
+#$conn = mysqli_connect('rdbms.strato.de', 'dbu1035725', 'Stoeptegel3!3107', 'dbs10066227');
+$conn = mysqli_connect('localhost', 'root','', 'php');
     if ($conn === false) {
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
--- welcome.php
+++ welcome.php
@@ -3,9 +3,58 @@ session_start();
 ?>
 <link rel="stylesheet" type="text/css" href="welcome.css">
 <title>Rick janssen</title>
+<body>
+<nav>
+  <a class="active" href="#">
+    <svg viewBox="0 0 100 100">
+      <g transform="translate(10 5) scale(0.8 0.9)">
+        <path d="M 0 30 v 70 h 100 v -70 l -50 -30 z" stroke="currentColor" stroke-width="10" fill="none"
+          stroke-linejoin="round" stroke-linecap="round" />
+      </g>
+    </svg>
+    <span>
+      Home
+    </span>
+  </a>
+  <a href="#">
+    <svg viewBox="0 0 100 100">
+      <g transform="translate(5 5) scale(0.9 0.9)">
+        <path d="M 50 35 a 20 20 0 0 1 50 0 q 0 25 -50 60 q -50 -35 -50 -60 a 25 25 0 0 1 50 0" stroke="currentColor"
+          stroke-width="10" fill="none" stroke-linejoin="round" stroke-linecap="round" />
+      </g>
+    </svg>
+    <span>
+      Likes
+    </span>
+  </a>
+
+  <a href="#">
+    <svg viewBox="0 0 100 100">
+      <g transform="translate(5 5) scale(0.9 0.9)">
+        <circle cx="45" cy="38" r="38" stroke="currentColor" stroke-width="10" fill="none" />
+        <line x1="66" y1="65" x2="100" y2="100" stroke="currentColor" stroke-width="10" />
+      </g>
+    </svg>
+    <span>
+      Search
+    </span>
+  </a>
+  <a href="">
+    <svg viewBox="0 0 100 100">
+      <g transform="translate(5 5) scale(0.9 0.9)">
+        <circle cx="50" cy="35" r="18" stroke="currentColor" stroke-width="10" fill="none" />
+        <rect x="15" y="75" width="70" height="50" rx="25" stroke="currentColor" stroke-width="10" fill="none" />
+      </g>
+    </svg>
+    <span>
+      Profile
+    </span>
+  </a>
+</nav>
+<script src="script.js"></script>
+</body>
 <?php
-session_start();
-include 'databaseconn.php';
+    include 'databaseconn.php';
     $result = mysqli_query($conn, 'SELECT * FROM `inlog`');
     $collection = [];
         if (mysqli_num_rows($result) > 0)
@@ -31,33 +80,16 @@ include 'databaseconn.php';
                 echo $foutmelding;
             }
         } 
-    $render  ='<h3><a href="http://stefanmars.nl">stefan mars</a></h3><br><br><br><br><br><br><br><br><br><br><br>';
-    $render .='<form action="index.php" method="post"><button type="submit" id ="logout" value ="logout" name ="logout">logout</button></form>';
-    $render .='<h3><a href= "colorgame.php">games</a></h3>';
-    echo $render;
+        $renderimg ='<a href="colorgame.php"><img src="colorgame.jpeg"style="max-width: 300px; max-height: 300px;/></a>';
+        echo $renderimg;
+        //$render ='<h3><a href="http://stefanmars.nl">stefan mars</a></h3>';
+       // $render .='<form action="index.php" method="post"><button type="submit" id ="logout" value ="logout" name ="logout">logout</button></form>';
+    
+        echo $render;
 
     if(!empty($post['logout'])){
         header('Location: https://rickjanssenic1e.nl');
         $_SESSION = [];
     }
-
-
-        // $htmlTable = "<table>";
-    // foreach ($collection as $value) {
-    //     $htmlTable .= "<tr><td class='info'>" . $value['id'] . "</td>";
-    //     $htmlTable .= "<td class='info'>" . $value['username'] . "</td>";
-    //     $htmlTable .= "<td class='info'>" . $value['password'] . "</td>";
-
-    // }
-    // $htmlTable .= "</table>";
-
-    //echo ($htmlTable);
-    //echo "<pre>"; var_dump($_POST); echo "</pre>";
-    //echo "<pre>"; var_dump($collection); echo "</pre>";
-
-    // if(!empty($_POST['Login'])){ // check of op knop is gedrukt
-    // if (empty($_SESSION['status'])) {
-                    // echo md5($_POST["password"]);
-            // echo "- " . $person['password'];echo "<br>";
-            //}
-            //}
+    ?>
+    
