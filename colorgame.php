6add879fecd9b5bc9883a398067d095ec6ea5df0
Rick Janssen <123780835+Rick-Janssen@users.noreply.github.com>
2023-10-04 11:42:51 +0200

    Update colorgame.php

--- colorgame.php
+++ colorgame.php
@@ -24,7 +24,6 @@ session_start();
       <div id="left">
       <h2>leaderboard</h2><br><br>
       <?php
-         #$conn = mysqli_connect('rdbms.strato.de', 'dbu1035725', 'Stoeptegel3!3107', 'dbs10066227');
          $conn = mysqli_connect('localhost', 'root', '', 'php');
          if ($conn === false) {
             die("ERROR: Could not connect. " . mysqli_connect_error());
